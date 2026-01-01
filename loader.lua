a07b05f4653db685fcac7f12e28c89b57e44bd8e
WhoIsGenn <85014066+WhoIsGenn@users.noreply.github.com>
2026-01-01 15:16:34 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,11 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+task.wait(1.2)
+
+local WindUI = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/Footagesus/WindUI/main/main.lua"
+))()
+
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/WhoIsGenn/Fish-it/main/main.lua"
+))()(WindUI)
 
