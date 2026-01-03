688105efb8d435e2d03e208d4777af4996327e8f
WhoIsGenn <85014066+WhoIsGenn@users.noreply.github.com>
2026-01-03 07:30:17 +0700

    Update fishit.lua

--- fishit.lua
+++ fishit.lua
@@ -580,213 +580,6 @@ fishing:Slider({
     end
 })
 
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local c={d=false,e=1.6,f=0.37}
-
-local g=ReplicatedStorage:WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net")
-
-local h,i,j,k,l
-pcall(function()
-    h=g:WaitForChild("RF/ChargeFishingRod")
-    i=g:WaitForChild("RF/RequestFishingMinigameStarted")
-    j=g:WaitForChild("RE/FishingCompleted")
-    k=g:WaitForChild("RE/EquipToolFromHotbar")
-    l=g:WaitForChild("RF/CancelFishingInputs")
-end)
-
-local m=nil
-local n=nil
-local o=nil
-
-local function p()
-    task.spawn(function()
-        pcall(function()
-            local q,r=l:InvokeServer()
-            if not q then
-                while not q do
-                    local s=l:InvokeServer()
-                    if s then break end
-                    task.wait(0.05)
-                end
-            end
-
-            local t,u=h:InvokeServer(math.huge)
-            if not t then
-                while not t do
-                    local v=h:InvokeServer(math.huge)
-                    if v then break end
-                    task.wait(0.05)
-                end
-            end
-
-            i:InvokeServer(-139.63,0.996)
-        end)
-    end)
-
-    task.spawn(function()
-        task.wait(c.f)
-        if c.d then
-            pcall(j.FireServer,j)
-        end
-    end)
-end
-
-local function w()
-    n=task.spawn(function()
-        while c.d do
-            pcall(k.FireServer,k,1)
-            task.wait(1.5)
-        end
-    end)
-
-    while c.d do
-        p()
-        task.wait(c.e)
-        if not c.d then break end
-        task.wait(0.1)
-    end
-end
-
-local function x(y)
-    c.d=y
-    if y then
-        if m then task.cancel(m) end
-        if n then task.cancel(n) end
-        m=task.spawn(w)
-    else
-        if m then task.cancel(m) end
-        if n then task.cancel(n) end
-        m=nil
-        n=nil
-        pcall(l.InvokeServer,l)
-    end
-end
-
-netFolder = ReplicatedStorage:WaitForChild('Packages')
-    :WaitForChild('_Index')
-    :WaitForChild('sleitnick_net@0.2.0')
-    :WaitForChild('net')
-Remotes = {}
-Remotes.RF_RequestFishingMinigameStarted = netFolder:WaitForChild("RF/RequestFishingMinigameStarted")
-Remotes.RF_ChargeFishingRod = netFolder:WaitForChild("RF/ChargeFishingRod")
-Remotes.RF_CancelFising = netFolder:WaitForChild('RF/CancelFishingInputs')
-Remotes.RF_CancelFishing = netFolder:WaitForChild("RF/CancelFishingInputs")
-Remotes.chargeRod = netFolder:WaitForChild('RF/ChargeFishingRod')
-Remotes.RE_FishingCompleted = netFolder:WaitForChild("RE/FishingCompleted")
-Remotes.RF_AutoFish = netFolder:WaitForChild("RF/UpdateAutoFishingState")
-
-toggleState = {
-    autoFishing = false,
-    blatantRunning = false,
-}
-
-FishingController = require(
-    ReplicatedStorage:WaitForChild('Controllers')
-        :WaitForChild('FishingController')
-)
-
-local oldCharge = FishingController.RequestChargeFishingRod
-FishingController.RequestChargeFishingRod = function(...)
-    if toggleState.blatantRunning or toggleState.autoFishing then
-        return
-    end
-	return oldCharge(...)
-end
-
-local isAutoRunning = false
-
-local isSuperInstantRunning = false
-_G.ReelSuper = 1.15
-     toggleState.completeDelays = 0.30
-     toggleState.delayStart = 0.2
-    local function autoEquipSuper()
-        local success, err = pcall(function()
-            Remotes.RE_EquipTool:FireServer(1)
-        end)
-        if success then
-        end
-    end
-
-    local function superInstantFishingCycle()
-        task.spawn(function()
-            Remotes.RF_CancelFishing:InvokeServer()
-            Remotes.RF_ChargeFishingRod:InvokeServer(tick())
-            Remotes.RF_RequestFishingMinigameStarted:InvokeServer(-139.63796997070312, 0.9964792798079721)
-            task.wait(toggleState.completeDelays)
-            Remotes.RE_FishingCompleted:FireServer()
-        end)
-    end
-
-    local function doSuperFishingFlow()
-        superInstantFishingCycle()
-    end
-
-local function startSuperInstantFishing()
-    if isSuperInstantRunning then return end
-    isSuperInstantRunning = true
-
-    task.spawn(function()
-        while isSuperInstantRunning do
-            superInstantFishingCycle()
-            task.wait(math.max(_G.ReelSuper, 0.1))
-        end
-    end)
-end
-
-    local function stopSuperInstantFishing()
-        isSuperInstantRunning = false
-        print('Super Instant Fishing stopped')
-    end
-  
-fishing = Tab0:Section({ 
-    Title = "fishing X8 | Beta",
-    Icon = "fish",
-    TextTransparency = 0.05,
-    TextXAlignment = "Left",
-    TextSize = 17,
-})
-
-fishing:Toggle({
-    Title = "Blatant Mode",
-    Value = toggleState.blatantRunning,
-    Callback = function(value)
-        toggleState.blatantRunning = value
-        Remotes.RF_AutoFish:InvokeServer(value)
-
-        if value then
-            startSuperInstantFishing()
-        else
-            stopSuperInstantFishing()
-        end
-    end
-})
-
-fishing:Input({
-    Title = "Reel Delay",
-    Placeholder = "Delay (seconds)",
-    Default = tostring(_G.ReelSuper),
-    Callback = function(input)
-        local num = tonumber(input)
-        if num and num >= 0 then
-            _G.ReelSuper = num
-            print("ReelSuper updated to:", num)
-        end
-    end
-})
-
-fishing:Input({
-    Title = "Custom Complete Delay",
-    Placeholder = "Delay (seconds)",
-    Default = tostring(toggleState.completeDelays),
-    Callback = function(input)
-        local num = tonumber(input)
-        if num and num > 0 then
-            toggleState.completeDelays = num
-        end
-    end
-})
-
 item = Tab3:Section({     
     Title = "Item",
     Icon = "list-collapse",
@@ -1079,6 +872,233 @@ event:Toggle({
 local Tab0 = Window:Tab({
     Title = "Webhook",
     Icon = "star",
+})
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local c={d=false,e=1.6,f=0.37}
+
+local g=ReplicatedStorage:WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_net@0.2.0"):WaitForChild("net")
+
+local h,i,j,k,l
+pcall(function()
+    h=g:WaitForChild("RF/ChargeFishingRod")
+    i=g:WaitForChild("RF/RequestFishingMinigameStarted")
+    j=g:WaitForChild("RE/FishingCompleted")
+    k=g:WaitForChild("RE/EquipToolFromHotbar")
+    l=g:WaitForChild("RF/CancelFishingInputs")
+end)
+
+local m=nil
+local n=nil
+local o=nil
+
+local function p()
+    task.spawn(function()
+        pcall(function()
+            local q,r=l:InvokeServer()
+            if not q then
+                while not q do
+                    local s=l:InvokeServer()
+                    if s then break end
+                    task.wait(0.05)
+                end
+            end
+
+            local t,u=h:InvokeServer(math.huge)
+            if not t then
+                while not t do
+                    local v=h:InvokeServer(math.huge)
+                    if v then break end
+                    task.wait(0.05)
+                end
+            end
+
+            i:InvokeServer(-139.63,0.996)
+        end)
+    end)
+
+    task.spawn(function()
+        task.wait(c.f)
+        if c.d then
+            pcall(j.FireServer,j)
+        end
+    end)
+end
+
+local function w()
+    n=task.spawn(function()
+        while c.d do
+            pcall(k.FireServer,k,1)
+            task.wait(1.5)
+        end
+    end)
+
+    while c.d do
+        p()
+        task.wait(c.e)
+        if not c.d then break end
+        task.wait(0.1)
+    end
+end
+
+local function x(y)
+    c.d=y
+    if y then
+        if m then task.cancel(m) end
+        if n then task.cancel(n) end
+        m=task.spawn(w)
+    else
+        if m then task.cancel(m) end
+        if n then task.cancel(n) end
+        m=nil
+        n=nil
+        pcall(l.InvokeServer,l)
+    end
+end
+
+netFolder = ReplicatedStorage:WaitForChild('Packages')
+    :WaitForChild('_Index')
+    :WaitForChild('sleitnick_net@0.2.0')
+    :WaitForChild('net')
+Remotes = {}
+Remotes.RF_RequestFishingMinigameStarted = netFolder:WaitForChild("RF/RequestFishingMinigameStarted")
+Remotes.RF_ChargeFishingRod = netFolder:WaitForChild("RF/ChargeFishingRod")
+Remotes.RF_CancelFising = netFolder:WaitForChild('RF/CancelFishingInputs')
+Remotes.RF_CancelFishing = netFolder:WaitForChild("RF/CancelFishingInputs")
+Remotes.chargeRod = netFolder:WaitForChild('RF/ChargeFishingRod')
+Remotes.RE_FishingCompleted = netFolder:WaitForChild("RE/FishingCompleted")
+Remotes.RF_AutoFish = netFolder:WaitForChild("RF/UpdateAutoFishingState")
+
+toggleState = {
+    autoFishing = false,
+    blatantRunning = false,
+}
+
+FishingController = require(
+    ReplicatedStorage:WaitForChild('Controllers')
+        :WaitForChild('FishingController')
+)
+
+local oldCharge = FishingController.RequestChargeFishingRod
+FishingController.RequestChargeFishingRod = function(...)
+    if toggleState.blatantRunning or toggleState.autoFishing then
+        return
+    end
+	return oldCharge(...)
+end
+
+local isAutoRunning = false
+
+local isSuperInstantRunning = false
+_G.ReelSuper = 1.15
+     toggleState.completeDelays = 0.30
+     toggleState.delayStart = 0.2
+    local function autoEquipSuper()
+        local success, err = pcall(function()
+            Remotes.RE_EquipTool:FireServer(1)
+        end)
+        if success then
+        end
+    end
+
+    local function superInstantFishingCycle()
+        task.spawn(function()
+            Remotes.RF_CancelFishing:InvokeServer()
+            Remotes.RF_ChargeFishingRod:InvokeServer(tick())
+            Remotes.RF_RequestFishingMinigameStarted:InvokeServer(-139.63796997070312, 0.9964792798079721)
+            task.wait(toggleState.completeDelays)
+            Remotes.RE_FishingCompleted:FireServer()
+        end)
+    end
+
+    local function doSuperFishingFlow()
+        superInstantFishingCycle()
+    end
+
+local function startSuperInstantFishing()
+    if isSuperInstantRunning then return end
+    isSuperInstantRunning = true
+
+    task.spawn(function()
+        while isSuperInstantRunning do
+            superInstantFishingCycle()
+            task.wait(math.max(_G.ReelSuper, 0.1))
+        end
+    end)
+end
+
+    local function stopSuperInstantFishing()
+        isSuperInstantRunning = false
+        print('Super Instant Fishing stopped')
+    end
+  
+blantant = Tab0:Section({ 
+    Title = "Blantant X8 | Recomended",
+    Icon = "fish",
+    TextTransparency = 0.05,
+    TextXAlignment = "Left",
+    TextSize = 17,
+})
+
+blantant:Toggle({
+    Title = "Blatant Mode",
+    Value = toggleState.blatantRunning,
+    Callback = function(value)
+        toggleState.blatantRunning = value
+        Remotes.RF_AutoFish:InvokeServer(value)
+
+        if value then
+            startSuperInstantFishing()
+        else
+            stopSuperInstantFishing()
+        end
+    end
+})
+
+blantant:Input({
+    Title = "Reel Delay",
+    Placeholder = "Delay (seconds)",
+    Default = tostring(_G.ReelSuper),
+    Callback = function(input)
+        local num = tonumber(input)
+        if num and num >= 0 then
+            _G.ReelSuper = num
+            print("ReelSuper updated to:", num)
+        end
+    end
+})
+
+blantant:Input({
+    Title = "Custom Complete Delay",
+    Placeholder = "Delay (seconds)",
+    Default = tostring(toggleState.completeDelays),
+    Callback = function(input)
+        local num = tonumber(input)
+        if num and num > 0 then
+            toggleState.completeDelays = num
+        end
+    end
+})
+
+Tab0:Space()
+
+blantant:Button({
+    Title = "X5 V1",
+    Desc = "This Is X5 In Free Script",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://pastefy.app/pIoVjvmR/raw"))()
+    end
+})
+
+blantant:Button({
+    Title = "X7 OLD",
+    Desc = "OLD METHOD X7",
+    Locked = false,
+    Callback = function()
+        loadstring(game:HttpGet("https://pastefy.app/lrOQwepH/raw"))()
+    end
 })
 
 webhook = Tab0:Section({ 
@@ -1962,7 +1982,7 @@ local Tab7 = Window:Tab({
 })
 
 player = Tab7:Section({ 
-    Title = "Player In Game",
+    Title = "Player Featured",
     Icon = "play",
     TextXAlignment = "Left",
     TextSize = 17,
@@ -2300,7 +2320,7 @@ task.spawn(function()
 end)
 
 graphic = Tab7:Section({ 
-    Title = "Graphics In Game",
+    Title = "Graphics Featured",
     Icon = "chart-bar",
     TextXAlignment = "Left",
     TextSize = 17,
